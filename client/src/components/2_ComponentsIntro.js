3dab08c5a15ef83d5f272bf1b853156f9e0a0ebd
Paul O'Connor <jamespauloconnor@gmail.com>
2016-12-29 21:50:42 -0500

    Components intro

--- client/src/components/2_ComponentsIntro.js
+++ client/src/components/2_ComponentsIntro.js
@@ -1,19 +1,23 @@
+//TODO Add more to this list.
 import React from 'react';
 
 class ComponentsIntro extends React.Component {
   render() {
     return (
     	<div>
-    		<h1>Components Intro</h1>
+    		<h3>Components Intro</h3>
     		<ul>
                 <li>Let you split the UI into independent, reusable pieces</li>
-                <li>Allow for isolation of each piece.</li>
+                <li>Allow isolation of each piece.</li>
                 <li>There is a Component Lifecycle, which we'll discuss.</li>
-                <li>All Custom Components should start with a capital letter</li>
-                <li>Right now, we're creating instances of components in app.js</li>
+                <li>All Custom Components should start with capital letter</li>
+                <li>Right now, we're creating instances of components index.js</li>
             </ul> 
             <hr/>
+
     	</div>
     );
   }
 }
+
+export default ComponentsIntro;
