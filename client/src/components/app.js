c80c6df1afeb111c2394be6ed32dc747c2930678
Paul O'Connor <jamespauloconnor@gmail.com>
2016-12-30 20:01:39 -0500

    First component and Navbar added

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import NavBarHeader from './Nav';
 
 	export default class App extends Component {
   		render() {
     			return (
-      			<div>React Hello World</div>
+      			<div>
+	      			<NavBarHeader />
+      			</div>
     			);
   		}
 	}
