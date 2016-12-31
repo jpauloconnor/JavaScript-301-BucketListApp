1f64b1f1d5b87d255f2aebac16b5b3329ea38652
Paul O'Connor <jamespauloconnor@gmail.com>
2016-12-30 22:13:32 -0500

    Added full to do list app

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import NavBarHeader from './Nav';
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
 	export default class App extends Component {
   		render() {
     			return (
       			<div>
 	      			<NavBarHeader />
+	      			<AddTodo />
+   					<VisibleTodoList />
+    				<Footer />
       			</div>
     			);
   		}
