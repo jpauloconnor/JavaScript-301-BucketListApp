d8f573e133a6cc2e596a1ee40abc565089c9e46d
Paul O'Connor <jamespauloconnor@gmail.com>
2017-02-15 21:45:28 -0500

    clean up

--- server/router.js
+++ server/router.js
@@ -3,9 +3,6 @@ var User = require('./models/user');
 
 
 module.exports = function(app){
-	app.get('/', function(req, res, next){
-		res.send("HELLO HOMEPAGE");		
-	});
 
 	app.post('/signup', Auth.signup);
 
