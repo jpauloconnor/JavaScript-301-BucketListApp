f1def7c2ffa46461e256ca60586a98b786bc3b1c
Paul O'Connor <jamespauloconnor@gmail.com>
2017-02-16 23:01:16 -0500

    Added post actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,43 @@
-import { ADD_TODO } from './types';
+import axios from 'axios';
+import { 
+  FETCH_POSTS,
+  CREATE_POSTS,
+  FETCH_POST,
+  DELETE_POST
+ } from './types';
 
-let nextTodoId = 0
-export const addTodo = (text) => {
-  return {
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text: text
-  }
+
+export function fetchPosts() {
+
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+	return {
+		type: FETCH_POSTS,
+		payload: request
+	};
 }
 
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+	return {
+		type: CREATE_POSTS,
+		payload: request
+	};
+}
+
+export function fetchPost(id) {
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+	return {
+		type: FETCH_POST,
+		payload: request
+	};
+}
+
+export function deletePost(id) {
+	const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+	return {
+		type: DELETE_POST,
+		payload: request
+	};
+}
