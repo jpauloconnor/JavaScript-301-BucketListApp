e442ae5696f6fdb23bcd4e718a55e686cf42f1ae
Paul O'Connor <jamespauloconnor@gmail.com>
2017-02-26 08:10:57 -0500

    Added code for fetching posts

--- server/controllers/bucketlistcontroller.js
+++ server/controllers/bucketlistcontroller.js
@@ -3,10 +3,10 @@ var BucketList = require('../models/bucketlist.js');
 exports.addBucketList = function(req, res, next){  
     //For Postman use
     //var title = req.body.title;    
-    var title = req.body.props.title;
-    var topic = req.body.props.topic;
-    var url = req.body.props.url;
-    var content = req.body.props.content;
+    var title = req.body.title;
+    var topic = req.body.topic;
+    var url = req.body.url;
+    var content = req.body.content;
     var specificUser = req.user;
 
     var bucketList = new BucketList({
@@ -21,4 +21,18 @@ exports.addBucketList = function(req, res, next){
         if(err) { return next(err); }
         res.json(bucketList);
     });
+}
+
+
+exports.fetchBucketLists = function(req, res) {
+  var specificUser = req.user._id;
+  BucketList.find({specificUser: specificUser})
+  .then(
+    function fetchSuccess(data) {
+      res.json(data);
+    },
+    function fetchError(err) {
+      res.send(500, err.message);
+    }
+  );
 }
