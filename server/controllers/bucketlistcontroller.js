a1b871e45a5c95c6c1e96f8ba7b9fa4604bc2ae3
Paul O'Connor <jamespauloconnor@gmail.com>
2017-02-26 08:17:50 -0500

    Added server fetch code

--- server/controllers/bucketlistcontroller.js
+++ server/controllers/bucketlistcontroller.js
@@ -3,10 +3,10 @@ var BucketList = require('../models/bucketlist.js');
 exports.addBucketList = function(req, res, next){  
     //For Postman use
     //var title = req.body.title;    
-    var title = req.body.title;
-    var topic = req.body.topic;
-    var url = req.body.url;
-    var content = req.body.content;
+    var title = req.body.props.title;
+    var topic = req.body.props.topic;
+    var url = req.body.props.url;
+    var content = req.body.props.content;
     var specificUser = req.user;
 
     var bucketList = new BucketList({
